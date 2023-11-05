af15d2d4a5bc226773eeebce747d07e013406315
Han Huynh Gia <94831616+hg27haan@users.noreply.github.com>
2023-11-05 15:24:50 +0700

    loi me

--- UC_Admin_Account.cs
+++ UC_Admin_Account.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,21 @@ namespace QuanLyChuoiQuanCaPhe
 {
     public partial class UC_Admin_Account : UserControl
     {
+
+        SqlConnection conn = new SqlConnection(Properties.Settings.Default.connStr);
         public UC_Admin_Account()
         {
             InitializeComponent();
+        }
+
+        private void UC_Admin_Account_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Account");
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvAccount.DataSource = dt;
+
         }
     }
 }
