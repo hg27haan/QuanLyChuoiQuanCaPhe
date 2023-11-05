84fea051560b09c1733bb74068f722c5737a5f7e
NTDHaruto <dung11112003qng@gmail.com>
2023-11-05 16:44:42 +0700

    Vừa qua phần Ca làm việc của quản lý

--- UC_QL_CaLamViec.cs
+++ UC_QL_CaLamViec.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,50 @@ namespace QuanLyChuoiQuanCaPhe
 {
     public partial class UC_QL_CaLamViec : UserControl
     {
+        SqlConnection conn = new SqlConnection(Properties.Settings.Default.connStr);
+
         public UC_QL_CaLamViec()
         {
             InitializeComponent();
+        }
+
+        private void doiTenHeaderCLV()
+        {
+            gvCaLamViec.Columns[0].HeaderText = "Mã Ca Làm Việc";
+            gvCaLamViec.Columns[1].HeaderText = "Giờ Bắt Đầu";
+            gvCaLamViec.Columns[2].HeaderText = "Giờ Kết Thúc";
+        }
+
+        private void loadThongTinCLV()
+        {
+            try
+            {
+                conn.Open();
+
+                string query = "select *from V_CaLamViec";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                gvCaLamViec.DataSource = dataTable;
+
+                doiTenHeaderCLV();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnXemCLV_Click(object sender, EventArgs e)
+        {
+            loadThongTinCLV();
         }
     }
 }
--- UC_QL_CaLamViec.designer.cs
+++ UC_QL_CaLamViec.designer.cs
@@ -28,28 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gvCaLamViec = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dptNgayLam = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
             this.txtMaCLV = new System.Windows.Forms.TextBox();
             this.txtMaNV = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.dptNgayLam = new System.Windows.Forms.DateTimePicker();
             this.btnSuaCLV = new System.Windows.Forms.Button();
             this.btnXoaCLV = new System.Windows.Forms.Button();
             this.btnThemCLV = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.gvCaLamViec)).BeginInit();
+            this.gvCaLamViec = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCaLamViec)).BeginInit();
             this.SuspendLayout();
-            // 
-            // gvCaLamViec
-            // 
-            this.gvCaLamViec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvCaLamViec.Location = new System.Drawing.Point(13, 13);
-            this.gvCaLamViec.Name = "gvCaLamViec";
-            this.gvCaLamViec.Size = new System.Drawing.Size(1073, 327);
-            this.gvCaLamViec.TabIndex = 1;
             // 
             // groupBox1
             // 
@@ -67,24 +63,40 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông Tin Ca Làm";
             // 
+            // dptNgayLam
+            // 
+            this.dptNgayLam.Location = new System.Drawing.Point(334, 58);
+            this.dptNgayLam.Name = "dptNgayLam";
+            this.dptNgayLam.Size = new System.Drawing.Size(271, 23);
+            this.dptNgayLam.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(331, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ngày Làm:";
+            // 
             // txtMaCLV
             // 
-            this.txtMaCLV.Location = new System.Drawing.Point(124, 123);
+            this.txtMaCLV.Location = new System.Drawing.Point(126, 95);
             this.txtMaCLV.Name = "txtMaCLV";
-            this.txtMaCLV.Size = new System.Drawing.Size(190, 23);
+            this.txtMaCLV.Size = new System.Drawing.Size(156, 23);
             this.txtMaCLV.TabIndex = 8;
             // 
             // txtMaNV
             // 
-            this.txtMaNV.Location = new System.Drawing.Point(124, 59);
+            this.txtMaNV.Location = new System.Drawing.Point(126, 58);
             this.txtMaNV.Name = "txtMaNV";
-            this.txtMaNV.Size = new System.Drawing.Size(190, 23);
+            this.txtMaNV.Size = new System.Drawing.Size(156, 23);
             this.txtMaNV.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 126);
+            this.label2.Location = new System.Drawing.Point(8, 98);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(112, 16);
             this.label2.TabIndex = 1;
@@ -93,33 +105,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 62);
+            this.label1.Location = new System.Drawing.Point(8, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã Nhân Viên:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(341, 23);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 16);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Ngày Làm:";
-            // 
-            // dptNgayLam
-            // 
-            this.dptNgayLam.Location = new System.Drawing.Point(344, 44);
-            this.dptNgayLam.Name = "dptNgayLam";
-            this.dptNgayLam.Size = new System.Drawing.Size(271, 23);
-            this.dptNgayLam.TabIndex = 10;
-            // 
             // btnSuaCLV
             // 
             this.btnSuaCLV.BackColor = System.Drawing.Color.Lime;
             this.btnSuaCLV.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSuaCLV.Location = new System.Drawing.Point(778, 470);
+            this.btnSuaCLV.Location = new System.Drawing.Point(830, 484);
             this.btnSuaCLV.Name = "btnSuaCLV";
             this.btnSuaCLV.Size = new System.Drawing.Size(229, 78);
             this.btnSuaCLV.TabIndex = 7;
@@ -149,27 +145,67 @@
             this.btnThemCLV.Text = "Thêm Ca Làm Việc Cho Nhân Viên";
             this.btnThemCLV.UseVisualStyleBackColor = false;
             // 
+            // gvCaLamViec
+            // 
+            this.gvCaLamViec.AllowUserToAddRows = false;
+            this.gvCaLamViec.AllowUserToDeleteRows = false;
+            this.gvCaLamViec.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gvCaLamViec.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.gvCaLamViec.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle13.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle13.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle13.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle13.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle13.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gvCaLamViec.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle13;
+            this.gvCaLamViec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle14.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle14.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle14.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle14.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gvCaLamViec.DefaultCellStyle = dataGridViewCellStyle14;
+            this.gvCaLamViec.Location = new System.Drawing.Point(13, 15);
+            this.gvCaLamViec.MultiSelect = false;
+            this.gvCaLamViec.Name = "gvCaLamViec";
+            this.gvCaLamViec.ReadOnly = true;
+            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle15.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle15.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle15.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle15.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gvCaLamViec.RowHeadersDefaultCellStyle = dataGridViewCellStyle15;
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.gvCaLamViec.RowsDefaultCellStyle = dataGridViewCellStyle16;
+            this.gvCaLamViec.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gvCaLamViec.Size = new System.Drawing.Size(1073, 322);
+            this.gvCaLamViec.TabIndex = 9;
+            // 
             // UC_QL_CaLamViec
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gvCaLamViec);
             this.Controls.Add(this.btnSuaCLV);
             this.Controls.Add(this.btnXoaCLV);
             this.Controls.Add(this.btnThemCLV);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.gvCaLamViec);
             this.Name = "UC_QL_CaLamViec";
             this.Size = new System.Drawing.Size(1099, 595);
-            ((System.ComponentModel.ISupportInitialize)(this.gvCaLamViec)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCaLamViec)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView gvCaLamViec;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DateTimePicker dptNgayLam;
         private System.Windows.Forms.Label label3;
@@ -180,5 +216,6 @@
         private System.Windows.Forms.Button btnSuaCLV;
         private System.Windows.Forms.Button btnXoaCLV;
         private System.Windows.Forms.Button btnThemCLV;
+        private System.Windows.Forms.DataGridView gvCaLamViec;
     }
 }
